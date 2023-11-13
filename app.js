c4c69bd880833265a2c1ee63e570953bebb917b2
palmaz99 <143984439+palmaz99@users.noreply.github.com>
2023-11-13 05:50:14 +0000

    new_working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,17 +12,7 @@ var loginRouter = require('./routes/login');
 
 var app = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://webLab:Suralu33322@weblab.qojan6v.mongodb.net/patientsApp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
+mongoose.connect('mongodb+srv://webLab:Suralu33322@weblab.qojan6v.mongodb.net/patientsApp')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,7 +32,30 @@ app.get('/login', (req, res) => {
   res.render('login'); // Render the login.ejs view
 });
 
-// ... (other routes)
+// Handle POST request for the login form
+app.post('/login', (req, res) => {
+  // Here, you can handle the login logic and authentication
+  const { username, password } = req.body;
+
+  // Perform authentication checks, e.g., validate username and password
+
+  // If authentication is successful, redirect to the main page
+  if (username === 'admin' && password === 'admin123') {
+    res.redirect('/index');
+  } else {
+    res.send('Invalid credentials'); // Render an error message if authentication fails
+  }
+});
+
+// Define a route for the main page
+app.get('/index', (req, res) => {
+  res.render('index'); // Render the index.ejs view
+});
+
+// Define a route for the create-patient page
+app.get('/create-patient', (req, res) => {
+  res.render('create-patient'); // Render the create-patient.ejs view
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
