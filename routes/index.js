866f0103ec0666584be31fa859d22d56d79e053d
Sumer7121 <143977296+Sumer7121@users.noreply.github.com>
2023-11-13 21:33:59 +0000

    home page update

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Patient = require('../models/patients');
-const { findByIdAndRemove } = require('../models/patients');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  // let newPatient = new Patient({
-  //   "CreatorId": "12345",
-  //   "CreatorName": "John Doe",
-  //   "FirstName": "Alice",
-  //   "LastName": "Smith",
-  //   "Birthdate": "1990-05-15",
-  //   "Zipcode": "12345",
-  //   "State": "California",
-  //   "PhoneNumber": "555-1234",
-  //   "CreateDate": "2023-11-10",
-  //   "InsuranceType": "Health Insurance",
-  //   "TestType": "Blood Test",
-  //   "DoctorService": "General Check-up",
-  //   "LabName": "ABC Lab",
-  //   "SampleStatus": "Pending"
-  // });
-
-  // try {
-  //   await newPatient.save();
-  // } catch (err) {
-  //   console.log(err);
-  // }
-
-  res.render('index', { title: 'Express' });
+  try {
+    const patients = await Patient.find(); // Fetch all patient records from the database
+    res.render('index', { title: 'Express', patients }); // Pass the patients data to the template
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
