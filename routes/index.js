66367c022dd712868f4e42f84c33a51ac248d108
Sumer7121 <143977296+Sumer7121@users.noreply.github.com>
2023-11-27 04:52:15 +0000

    delete button working

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,20 @@ const Patient = require('../models/patients');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const patients = await Patient.find(); // Fetch all patient records from the database
-    res.render('index', { title: 'Express', patients }); // Pass the patients data to the template
+    const patients = await Patient.find();
+    res.render('index', { title: 'Express', patients });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST route to handle patient deletion
+router.post('/delete-patient/:id', async function(req, res, next) {
+  const patientId = req.params.id;
+  try {
+    await Patient.findOneAndDelete(patientId);
+    console.log('Patient deleted successfully');
+    res.redirect('/');
   } catch (err) {
     next(err);
   }
